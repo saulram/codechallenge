0b04e00f1f495e86c032232a7add78e8934ec391
saulram <saul.ramirez@disolutionsmx.com>
2019-12-23 20:57:10 -0600

    api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var express = require('express');
-var path =require('path');
+var path = require('path');
 
 var bodyParser = require('body-parser');
 var app = express();
@@ -11,22 +11,22 @@ var invoice_routes = require('./routes/invoices.routes');
 
 
 //configuracion de bodyparser
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //configurar cabeceras
-app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers','Authorization,X-API-KEY,Origin,X-Requested-With,Content-Type,Accept,Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,DELETE');
-    res.header('Allow','GET,POST,OPTIONS,PUT,DELETE');
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization,X-API-KEY,Origin,X-Requested-With,Content-Type,Accept,Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE');
+    res.header('Allow', 'GET,POST,OPTIONS,PUT,DELETE');
     next();
-    
+
 });
+
 //rutas base
-app.use(express.static('./'));
-app.use('/api',invoice_routes);
-app.get('*',function(req,res,next){
+app.use('/api', invoice_routes);
+app.get('*', function (req, res, next) {
     res.sendFile(path.resolve('client/index.html'));
 
 });
